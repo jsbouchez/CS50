4811bf9e9d90f7596a3709b6abd5eebe3087a6ed
jsbouchez <juan.bouchez@outlook.com>
2025-09-15 17:47:56 -0300

    Add features to  hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-# Ask user for their names
-name = input("What's your name? ")
+# Ask user for their names, then remove spaces left and right and finally make the 1st letter of each word capital
+name = input("What's your name? ").strip().title()
 
-# Say hello to the user
-print("hello,", name)
+# Say hello to the user but using FORMATTED STRINGS
+print(f"hello, {name}")
